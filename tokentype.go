ca40dbb173a21e4452676fdc54d4e52a20135e7b
Oluwasanya Awe <olu@mac>
2023-06-25 23:24:38 -0400

    Add string representation of tokenType

--- tokentype.go
+++ tokentype.go
@@ -2,6 +2,92 @@ package main
 
 type tokenType int
 
+func (t tokenType) String() string {
+	switch t {
+	case LeftParen:
+		return "LeftParen"
+	case RightParen:
+		return "RightParen"
+	case LeftBrace:
+		return "LeftBrace"
+	case RightBrace:
+		return "RightBrace"
+	case Comma:
+		return "Comma"
+	case Dot:
+		return "Dot"
+	case Minus:
+		return "Minus"
+	case Plus:
+		return "Plus"
+	case Semicolon:
+		return "Semicolon"
+	case Slash:
+		return "Slash"
+	case Star:
+		return "Star"
+	case Bang:
+		return "Bang"
+	case BangEqual:
+		return "BangEqual"
+	case Equal:
+		return "Equal"
+	case EqualEqual:
+		return "EqualEqual"
+	case Greater:
+		return "Greater"
+	case GreaterEqual:
+		return "GreaterEqual"
+	case Less:
+		return "Less"
+	case LessEqual:
+		return "LessEqual"
+	case Identifier:
+		return "Identifier"
+	case String:
+		return "String"
+	case Number:
+		return "Number"
+	case And:
+		return "And"
+	case Class:
+		return "Class"
+	case Else:
+		return "Else"
+	case False:
+		return "False"
+	case Fun:
+		return "Fun"
+	case For:
+		return "For"
+	case If:
+		return "If"
+	case Nil:
+		return "Nil"
+	case Or:
+		return "Or"
+	case Print:
+		return "Print"
+	case Return:
+		return "Return"
+	case Super:
+		return "Super"
+	case This:
+		return "This"
+	case True:
+		return "True"
+	case Var:
+		return "Var"
+	case While:
+		return "While"
+	case EOF:
+		return "EOF"
+	default:
+		return "UnknownType"
+	}
+
+}
+
 const (
 	// Single-character tokens.
 	LeftParen tokenType = iota
